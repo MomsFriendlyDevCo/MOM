af2693a7ba4900713c7f31fc3a4e8777ecb04554
Matt Carter <m@ttcarter.com>
2022-12-22 16:26:27 +1000

    FIX: Nicer text reporting with summary at base

--- reporters/text.js
+++ reporters/text.js
@@ -27,12 +27,20 @@ export function run(responses, options) {
 		...settings.header(),
 		'',
 		...fails.map(m => `${settings.formatStatus(m.status)}: ${m.message}`),
-		fails.length > 0 ? '' : false,
-		fails.length > 0 ? '-----' : false,
-		fails.length > 0 ? '' : false,
+		...(fails.length > 0 && success.length > 0 // Do we have BOTH fails + successes? If so apply a padding bar
+			? [
+				'',
+				'-----',
+				'',
+			] : []
+		),
 		...success.map(m => `${m.status}: ${m.message}`),
+		success.length > 0 ? '' : false,
+		fails.length == 0 && success.length > 1 ? `All ${success.length} tests passing`
+			: fails.length > 0 && success.length > 0 ? `${fails.length} tests failing, ${success.length} succeeding out of ${responses.length} ~ ${Math.round((success.length / responses.length) * 100)}`
+			: fails.length > 1 ? `All ${fails.length} tests failing`
+			: false,
 		...settings.footer(),
-
 	]
 		.filter(v => v !== false)
 		.join('\n')
