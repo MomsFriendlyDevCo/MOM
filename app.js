ea7a4e0fd4c4005abc7d09d48ab79d5f9276dd0a
Matt Carter <m@ttcarter.com>
2022-12-23 10:39:29 +1000

    FEAT: Add `--json` shorthand for CLI

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import {Sanity} from '#lib/sanity';
 
 let opts = program
 	.name('sanity')
+	.option('-j, --json', 'Shorthand for `-r json`')
 	.option('-m, --module <mod=opt1:val1,opt2:val2>', 'Enable a module and set options (can specify multiple times)', (v, t) => {
 		let {module, args} = /^(?<module>.+?)(?:=(?<args>.+))?$/.exec(v).groups || {};
 		if (!module) throw new Error('Unknown module');
@@ -88,6 +89,7 @@ opts.module.forEach(({module, args}) =>
 // }}}
 
 // Load all reporters {{{
+if (opts.json) opts.reporter = [{reporter: 'json'}];
 if (!opts.reporter.length) opts.reporter = [{reporter: 'text'}]; // Default to 'text' if nothing else is given
 opts.reporter.forEach(({reporter, args}) =>
 	sanity.reporter(reporter, args)
