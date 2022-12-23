553343c241eb8f5e2dc756c97b1a9266e6447181
Matt Carter <m@ttcarter.com>
2022-12-23 10:00:47 +1000

    DOCS: JSON reporter options

--- reporters/json.js
+++ reporters/json.js
@@ -1,6 +1,12 @@
 import {inspect} from 'node:util';
 import jsonColorizer from 'json-colorizer';
 
+/**
+* Output responses as JSON
+* @param {Object} [options] Optional behaviour settings
+* @param {String|Number} [options.indent=2] Indent setting for standard JSON output
+* @param {String} [options.mode='prettyAuto'] How to output. ENUM: 'prettyAuto' (pretty only if STDOUT is a TTY), 'pretty' (force pretty mode), 'raw' (basic JSON output), 'object' JavaScript POJO
+*/
 export function run (responses, options) {
 	let settings = {
 		indent: 2,
