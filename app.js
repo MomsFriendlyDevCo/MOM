710c4734c263003761551fbf159ad68d41417156
Matt Carter <m@ttcarter.com>
2022-12-31 13:22:07 +1000

    REFACTOR: CLI now uses more logical order when loading modules / reporters - DO-WHAT-I-MEAN style

--- app.js
+++ app.js
@@ -36,16 +36,18 @@ let opts = program
 	.option('-l, --loop [times]', 'Repeat output the specifed number of times. Use "0" for forever', 1)
 	.option('-p, --loop-pause [timestring]', 'Wait for a timestring-compatible delay between each loop. Use "0" to disable', '10s')
 	.option('-v, --verbose', 'Be more verbose')
+	.option('--list-modules', 'List available modules (use --all to show unavailable ones too)')
+	.option('--all', 'Modifier for --list-modules / --list-reporters to show unavailable items')
 	.option('--no-env', 'Disable trying to read in config from .env files')
 	.option('--no-headers', 'Disable header seperators if multiple reporters return content')
 	.parse(process.argv)
 	.opts()
 
 let sanity = new Sanity();
-let loadedFromEnv = false;
 
-// Read .env files {{{
-if (opts.env) {
+// Load Modules {{{
+if (opts.env && opts.module.length == 0) {
+	// Load modules from .env* files {{{
 	let config = new DotEnv()
 		.parse([
 			'.env.example',
@@ -59,35 +61,59 @@ if (opts.env) {
 		.value();
 
 	if (opts.verbose) console.warn('Load module config', config);
-	if (Object.keys(config)) loadedFromEnv = true;
 
 	Object.entries(config)
 		.filter(([module, config]) => module && config.enabled)
 		.forEach(([module, config]) => {
-			if (opts.verbose) console.warn('Load module', module, config);
+			if (opts.verbose) console.warn(`Load MODULE:${module} config:`, config);
 			sanity.use(module, config)
 		})
+	// }}}
+} else if (opts.module.length > 0) {
+	// Load modules from CLI {{{
+	opts.module.forEach(({module, args}) =>
+		sanity.use(module, args)
+	);
+	// }}}
+} else {
+	throw new Error('Specify modules to load with `--module MOD` or create a .env file');
+}
+
+// Load Reporters {{{
+if (opts.env && opts.reporter.length == 0) {
+	// Load reporters from .env* files {{{
+	let config = new DotEnv()
+		.parse([
+			'.env.example',
+			'.env',
+		])
+		.schemaGlob(/\.ENABLED$/, Boolean) // Type cast all *_ENABLE values
+		.filterAndTrim(/^SANITY_REPORTER_/)
+		.toTree(/\./)
+		.deep()
+		.camelCase()
+		.value();
+
+	if (opts.verbose) console.warn('Load reporter config', config);
+
+	Object.entries(config)
+		.filter(([reporter, config]) => reporter && config.enabled)
+		.forEach(([reporter, config]) => {
+			if (opts.verbose) console.warn(`Load REPORTER:${reporter} config:`, config);
+			sanity.reporter(reporter, config)
+		})
+	// }}}
+} else if (opts.reporter.length > 0) {
+	// Load reporters from CLI {{{
+	opts.reporter.forEach(({reporter, args}) =>
+		sanity.reporter(reporter, args)
+	);
+	// }}}
+} else {
+	throw new Error('Specify reporters to load with `--reporter REPORTER` or create an .env file');
 }
 // }}}
 
-// Load all modules {{{
-if (!loadedFromEnv && !opts.module.length) opts.module = [ // Defaults if nothing else is given
-	// Can only use modules that dont require options
-	{module: 'diskSpaceTemp'},
-	{module: 'ping'},
-];
-opts.module.forEach(({module, args}) =>
-	sanity.use(module, args)
-);
-// }}}
-
-// Load all reporters {{{
-if (opts.json) opts.reporter = [{reporter: 'json'}];
-if (!opts.reporter.length) opts.reporter = [{reporter: 'text'}]; // Default to 'text' if nothing else is given
-opts.reporter.forEach(({reporter, args}) =>
-	sanity.reporter(reporter, args)
-);
-// }}}
 
 let runCount = 0;
 let loopPause = opts.loopPause == 0 ? 0 : timestring(opts.loopPause, 'ms');
@@ -108,7 +134,7 @@ let runner = ()=> Promise.resolve()
 			Object.entries(responses).forEach(([key, text]) => {
 				if (opts.headers) console.log(chalk.bold.bgBlue.black(` --- ${key} --- `));
 				console.log(text);
-				if (!text.endsWith('\n')) console.log(''); // Add newline if one isn't already present
+				if (typeof text == 'string' && !text.endsWith('\n')) console.log(''); // Add newline if one isn't already present
 			})
 		}
 	})
