b4b1b2e10e56d590e0d45c9b2d28a8b819f4284a
Matt Carter <m@ttcarter.com>
2022-12-31 13:37:39 +1000

    FEAT: MODULE:Memory

---
+++ modules/memory.js
@@ -0,0 +1,81 @@
+import fs from 'node:fs/promises';
+import {platform as osPlatform} from 'node:os';
+import readable from '@momsfriendlydevco/readable';
+
+let platform = osPlatform();
+
+export function config({Schema}) {
+	return new Schema({
+		memory: {type: Boolean, default: true},
+		memoryWarnPercent: {type: Number, default: 90},
+		memoryCritPercent: {type: Number, default: 100},
+
+		swap: {type: Boolean, default: true},
+		swapWarnPercent: {type: Number, default: 80},
+		swapCritPercent: {type: Number, default: 90},
+	});
+}
+
+export function isAvailable() {
+	if (!['darwin', 'freebsd', 'linux', 'openbsd', 'sunos'].includes(platform))
+		throw new Error('Cannot check diskspace on non Unix compatible systems');
+}
+
+export function run({options}) {
+	return fs.readFile('/proc/meminfo', 'utf-8')
+		.then(content => content.split('\n'))
+		.then(lines => lines
+			.map(line => {
+				let {key, val} =
+					/^(?<key>.+):\s*(?<val>\d+) kB$/.exec(line)?.groups || {};
+				return key
+					? [key, val * 1024]
+					: [];
+			})
+		)
+		.then(keyVals => Object.fromEntries(keyVals))
+		.then(keyVals =>
+			[
+				options.memory && {prefix: 'memory', title: 'Memory', keyFree: 'MemFree', keyTotal: 'MemTotal'},
+				options.swap && {prefix: 'swap', title: 'Swap', keyFree: 'SwapFree', keyTotal: 'SwapTotal'},
+			]
+				.filter(Boolean)
+				.map(field => {
+					let value = keyVals[field.keyFree];
+					let maxValue = keyVals[field.keyTotal];
+
+					if (!maxValue) throw new Error(`Cannot find maximum ("${field.keyTotal}") to match key "${field.keyFree}"`);
+					value = maxValue - value; // Switch MemoryFree -> MemoryUsed
+
+					let percentFree = (value / maxValue) * 100;
+					let status =
+						options[`${field.prefix}CritPercent`] && percentFree >= options[`${field.prefix}CritPercent`] ? 'CRIT'
+						: options[`${field.prefix}WarnPercent`] && percentFree >= options[`${field.prefix}WarnPercent`] ? 'WARN'
+						: 'OK';
+
+					return {
+						status,
+						field,
+						maxValue,
+						percent: Math.round((value / maxValue) * 100),
+						metric: {
+							id: field.prefix,
+							unit: 'bytes',
+							value,
+							warnValue: options[`${field.prefix}WarnPercent`] && `>=${options[`${field.prefix}WarnPercent`]}`,
+							critValue: options[`${field.prefix}CritPercent`] && `>=${options[`${field.prefix}CritPercent`]}`,
+						},
+					};
+				})
+		)
+		.then(stats => ({
+			status:
+				stats.some(m => m.status == 'CRIT') ? 'CRIT'
+				: stats.some(m => m.status == 'WARN') ? 'WARN'
+				: 'OK',
+			message: stats.map(m =>
+				`${m.field.title}: ${readable.fileSize(m.metric.value)} / ${readable.fileSize(m.maxValue)} ~ ${m.percent}%`,
+			).join(', '),
+			metric: stats.map(m => m.metric),
+		}))
+}
